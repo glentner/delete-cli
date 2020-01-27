44a006cd5f51d0ff1541003535f9496384b49635
Geoffrey Lentner <glentner@purdue.edu>
2020-01-27 16:12:50 -0500

    version 1.0.1

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2020 Geoffrey Lentner'
 author = 'Geoffrey Lentner <glentner@purdue.edu>'
 
 # The full version, including alpha/beta/rc tags
-release = '1.0.0'
+release = '1.0.1'
 
 
 # -- General configuration ---------------------------------------------------
